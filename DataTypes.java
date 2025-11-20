025426d1a4ab498b15afe2539c2f6b084a1c9746
Musharaf <musharafali.279@gmail.com>
2025-11-20 17:34:42 +0530

    Added variables

--- DataTypes.java
+++ DataTypes.java
@@ -1,5 +1,25 @@
 public class DataTypes {
     public static void main(String []args){
-        
+//  Declaring variables with data types
+    //Integer
+        byte by=127;//1 byte
+        short s=500;//2 bytes
+        int num=10;//4 bytes
+        long num2=3000000000L;//8 bytes
+    //Point values
+        float mark=85.7f;//4 bytes
+        double marks=90.5;//8 bytes
+    //Character
+        char c='m';//2 bytes
+    //Boolean
+        boolean b=true;
+        System.out.println(by);
+        System.out.println(s);
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(mark);
+        System.out.println(marks);
+        System.out.println(c);
+        System.out.println(b);
     }
 }
